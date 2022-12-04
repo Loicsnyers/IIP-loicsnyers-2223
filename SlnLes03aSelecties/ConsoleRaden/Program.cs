1d9de9be4dcb218a2eea5da3ef59535d5d982278
Loicsnyers <loic.snyers@gmail.com>
2022-12-04 21:02:37 +0100

    commit v2

---
+++ SlnLes03aSelecties/ConsoleRaden/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRaden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGokJuist = false;
+            int getal = 3;
+
+            while (!isGokJuist)
+            {
+                Console.Write("Raad een getal tussen 1 en 10:");
+                int gok = Convert.ToInt32(Console.ReadLine());
+
+                if (gok < getal)
+                {
+                    Console.WriteLine("de gok was te laag");
+                }
+                else if (gok > getal)
+                {
+                    Console.WriteLine("de gok was te hoog");
+                }
+                else
+                {
+                    Console.WriteLine("Juist, je hebt gewonnen");
+                    isGokJuist = true;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SlnLes03aSelecties/WpfSliderKleur/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSliderKleur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblValue.Content = Math.Round(lblSlider.Value);
+
+            if (lblSlider.Value <= 25) 
+            {
+                lblValue.Foreground= new SolidColorBrush(Colors.Green);
+            }
+            else if (lblSlider.Value <= 50)
+            {
+                lblValue.Foreground = new SolidColorBrush(Colors.Yellow);
+            }
+            else if (lblSlider.Value <= 75)
+            {
+                lblValue.Foreground = new SolidColorBrush(Colors.Orange);
+            }
+            else
+            {
+                lblValue.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            
+
+        }
+    }
+}
