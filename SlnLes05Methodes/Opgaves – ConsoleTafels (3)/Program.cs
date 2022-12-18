16f75539f50717760ff7c7dd170558b66fcceb30
Loicsnyers <loic.snyers@gmail.com>
2022-12-18 15:20:35 +0100

    ```````

---
+++ SlnLes05Methodes/Opgaves – ConsoleTafels (3)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgaves___ConsoleTafels__3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static private void DrukTafel(int get, int len)
+        {
+            Console.WriteLine("shit's makes no sense ");
+        }
+    }
+}
---
+++ SlnProject/Sequence game/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using WMPLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sequence_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+            player.URL = System.IO.Path.Combine(Environment.CurrentDirectory, "sounds/bradpittsound.mp3");
+
+            player.controls.play();
+            player.settings.volume = 40;
+        }
+    }
+}
