0a58b12cc22cbc5001bc9b9ca1b36162c1fd129c
Loicsnyers <loic.snyers@gmail.com>
2022-12-23 18:32:51 +0100

    sequence game push 3/3 before vacation
    disclaimer with fair use added
    forcing application exit after winning
    fixed sound always playing in the beginning , when program asked for how many sounds to generate
    removed sound[""].play
    added an error message when user generates less than 8 or more than 10 sounds.

--- SlnProject/Sequence game Console/Program.cs
+++ SlnProject/Sequence game Console/Program.cs
@@ -11,15 +11,26 @@ using System.IO;
 namespace Sequence_game_Console
 {
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
-
             string[] sounds = { "bradpittsound.wav", "kanyesound.wav", "christianbalesound.wav", "travissound.wav", "ryanreynoldssound.wav", "asaprocky.wav", "liluzi.wav", "chiefkeefsound.wav", "kawhisound.wav", "tobeymaguire.wav" };
             string[] soundAnswers = { "brad pitt", "kanye west", "christian bale", "travis scott", "ryan reynolds", "asap rocky", "lil uzi vert", "chief keef", "kawhi leonard", "tobey maguire" };
 
             int currentSound = 0; // keeps track of the sound that is playing
 
+            List<SoundPlayer> sound = new List<SoundPlayer>();
+
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\bradpittsound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\kanyesound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\christianbalesound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\travissound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\ryanreynoldssound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\asaprocky.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\liluzi.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\chiefkeefsound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\kawhisound.wav"));
+            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\tobeymaguire.wav"));
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(@" __        _______ _     ____ ___  __  __ _____   _____ ___    __  ____   __  ____  _____ ___  _   _ _____ _   _  ____ _____    ____    _    __  __ _____ _ 
@@ -28,8 +39,19 @@ namespace Sequence_game_Console
    \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| | | |  | | | |    ___) | |__| |_| | |_| | |___| |\  | |___| |___  | |_| |/ ___ \| |  | | |___|_|
     \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/  |_|  |_| |_|   |____/|_____\__\_\\___/|_____|_| \_|\____|_____|  \____/_/   \_\_|  |_|_____(_)");
 
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine();
             Console.WriteLine();
+
+            Console.WriteLine(@"DISCLAIMER!
+-----------
+These sounds are under Fair Use:
+Copyright Disclaimer Under Section 107 of the Copyright Act in 1976; Allowance is made for
+'Fair Use' for purposes such as criticism, comment, news reporting, teaching, scholarship, and research.
+Fair use is a use permitted by copyright statute that might otherwise be infringing. 
+Non-profit, educational or personal use tips the balance in favor of fair use.
+All rights and credit go directly to its rightful owners");
 
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -52,42 +74,18 @@ namespace Sequence_game_Console
 
             Console.WriteLine();
 
-            Console.Write("Before we know start the game i would like to know your name: ");
+            Console.Write("Before we start the game I would like to know your name: ");
             string name = Console.ReadLine();
             Console.Write($"Hello {name} how many sounds do you want to generate (8-10)? ");
             int generatedSounds = int.Parse(Console.ReadLine());
 
-            if (generatedSounds < 8 || generatedSounds > 10)
+            while (generatedSounds < 8 || generatedSounds > 10)
             {
                 Console.WriteLine("Invalid input. Number of sounds must be between 8 and 10.");
 
-                return;
+                Console.Write("Enter the number of sounds you want to generate(8-10):");
+                generatedSounds = int.Parse(Console.ReadLine());
             }
-
-            List<SoundPlayer> sound = new List<SoundPlayer>();
-
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\bradpittsound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\kanyesound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\christianbalesound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\travissound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\ryanreynoldssound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\asaprocky.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\liluzi.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\chiefkeefsound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\kawhisound.wav"));
-            sound.Add(new SoundPlayer(@"C:\Users\loics\source\repos\Loicsnyers\IIP-loicsnyers-2223\SlnProject\Sequence game Console\sounds\tobeymaguire.wav"));
-
-            sound[0].Play();
-            sound[1].Play();
-            sound[2].Play();
-            sound[3].Play();
-            sound[4].Play();
-            sound[5].Play();
-            sound[6].Play();
-            sound[7].Play();
-            sound[8].Play();
-            sound[9].Play();
-
 
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -96,7 +94,7 @@ namespace Sequence_game_Console
             Console.Write($"Awesome choice {name} you have chosen for {generatedSounds} sounds!\nTo start the game, type 'ready' and press enter:");
 
             string input = string.Empty;
-            while (input.Trim() != "ready") // when user did spacebar before ready, the console didn't clear so i used the input.trim() .trim removes all leading and trailing whitespaces from the current string.
+            while (input.Trim() != "ready") // when user did spacebar before ready, the console didn't clear so i used the input.trim .trim removes all leading and trailing whitespaces fro the current string
             {
                 input = Console.ReadLine().ToLower();
             }
@@ -106,16 +104,16 @@ namespace Sequence_game_Console
 
             Console.ForegroundColor = ConsoleColor.Green;
 
+            int score = 0; // code missing for hi scores file;
             while (true)
             {
-                StartCountdown("press enter to start the countdown...", 3);
-
                 int correctGuesses = 0;
                 for (int i = 0; i < generatedSounds; i++)
                 {
-                    Console.WriteLine($"sound {currentSound + 1} is playing...", i + 1);
-                    sound[i].Play();
+                    StartCountdown("press enter to start the countdown...", 3);
 
+                    Console.WriteLine($"sound {currentSound + 1} is playing...");
+                    sound[i].Play();
 
                     System.Threading.Thread.Sleep(750);
 
@@ -141,15 +139,18 @@ namespace Sequence_game_Console
                         SoundPlayer laugh = new SoundPlayer(path);
                         laugh.Load();
                         laugh.PlaySync();
+                        currentSound = i;
+                        correctGuesses = i;
                         Console.WriteLine();
-                        Console.WriteLine("you Lost, press enter to play again");
+                        Console.WriteLine($"you Lost, you guessed {correctGuesses} out of {generatedSounds} sounds. \npress enter to play again");
                         Console.ReadLine();
                         Console.Clear();
                     }
+
                     if (correctGuesses == generatedSounds)
                     {
                         Console.Clear();
-                        Console.WriteLine(@"         _nnnn_                      
+                        Console.WriteLine(@"         _nnnn_ 
         dGGGGMMb     ,"""""""""""""""""""""""""""".
        @p~qp~~qMb    |   You won  ! |
        M|@||@) M|   _;..............'
@@ -169,6 +170,7 @@ _)      \.___.,|     .'
                         SoundPlayer victory = new SoundPlayer(path);
                         victory.Load();
                         victory.PlaySync();
+                        Environment.Exit(0);
                     }
                 }
             }
